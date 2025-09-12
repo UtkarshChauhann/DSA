107ffc73296bb8fb5fe1423da6e2b44cba29fe62
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2025-09-12 16:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (34.93%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.java
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -1,9 +1,14 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        int m = Integer.MAX_VALUE;
-        for(int i=1; i<m; i++){
-            if(i%2 == 0 && i%n == 0) return i;
-        }
+        // int m = Integer.MAX_VALUE;
+        // for(int i=1; i<m; i++){
+        //     if(i%2 == 0 && i%n == 0) return i;
+        // }
+        // return -1;
+
+        if(n%2 == 0) return n;
+        if(n%2 == 1) return 2*n;
+
         return -1;
     }
 }
