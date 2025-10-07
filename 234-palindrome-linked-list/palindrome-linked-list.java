8ec8c43cfb4571e30d98c2d5528e5ddc06c9ef19
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2025-10-07 16:44:37 +0530

    Time: 3 ms (99.76%) | Memory: 68.7 MB (39.50%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,27 +1,15 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode newHead = new ListNode(head.val);
-        ListNode t1 = newHead;
-        ListNode t2 = head.next;
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while(t2 != null){
-            ListNode temp = new ListNode(t2.val);
-            t1.next = temp;
-            t1 = t1.next;
-            t2 = t2.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        ListNode curr = head;
+        ListNode curr = slow;
         ListNode prev = null;
         ListNode nxt = null;
 
@@ -31,19 +19,17 @@ class Solution {
             prev = curr;
             curr = nxt;
         }
-        head = prev;
         
-        t1 = newHead;
-        t2 = head;
 
-        while(t1 != null){
+        ListNode t1 = head;
+        ListNode t2 = prev;
+
+        while(t2 != null){
             if(t1.val != t2.val) return false;
-
             t1 = t1.next;
             t2 = t2.next;
         }
 
         return true;
-
     }
 }
