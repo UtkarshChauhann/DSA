2c1874754b47daefcacc9bd15b5a0f18cf63f70c
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2025-10-13 12:19:39 +0530

    Time: 1 ms (93.81%) | Memory: 41.3 MB (35.46%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxProduct(int n) {
+        int mx1 = -1;
+        int mx2 = -1;
+        int[] freq = new int[10];
+
+        while(n != 0){
+            int digit = n%10;
+            freq[digit]++;
+
+            if(digit > mx1){
+                mx2 = mx1;
+                mx1 = digit;
+            }else if(digit > mx2 && digit < mx1){
+                mx2 = digit;
+            }
+            n /= 10;
+            if(n%10 == mx1){
+                mx2 = mx1;
+
+            }
+        }
+
+        if(freq[mx1] >=2) return mx1*mx1;
+
+        return mx1*mx2;
+
+    }
+}
