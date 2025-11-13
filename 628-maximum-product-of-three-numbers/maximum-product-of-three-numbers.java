6db3d39d648bb6619c656c919546032a5871c67f
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2025-11-13 11:03:48 +0530

    Time: 2 ms (99.42%) | Memory: 47.7 MB (15.21%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,38 +1,30 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
 
-        int n = nums.length;
+        for (int n : nums) {
+            // Track top 3 maximums
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
 
-        int prod1 = nums[n-3]*nums[n-2]*nums[n-1];
-        int prod2 = nums[0]*nums[1]*nums[n-1];
+            // Track bottom 2 minimums
+            if (n < min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
+        }
 
-        return Math.max(prod1, prod2);
-
-
-
-
-
-
-        
-        // int n = nums.length;
-        // int cnt = 1, prod = 1;
-        // int ans = Integer.MIN_VALUE;
-
-        // for(int i=0; i<n-1; i++){
-        //     prod = nums[i];
-        //     for(int j=i+1; j<n; j++){
-        //         prod *= nums[j];
-        //         cnt++;
-        //         ans = Math.max(ans, prod);
-
-        //         if(cnt == 3){
-        //             cnt = 1;
-        //             prod = nums[i];
-        //         }
-        //     }
-        // }
-
-        // return ans;
+        return Math.max(max1 * max2 * max3, min1 * min2 * max1);
     }
 }
