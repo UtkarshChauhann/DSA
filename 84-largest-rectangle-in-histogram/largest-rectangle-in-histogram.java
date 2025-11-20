315883592c1a1d95208238b6268d7485b339440c
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2025-11-20 14:50:14 +0530

    Time: 106 ms (32.80%) | Memory: 77.5 MB (19.82%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        Stack<Integer> st = new Stack<>();
+
+        int[] nse = new int[n]; //next smallest ele
+        int[] pse = new int[n]; //prev  -  -   
+
+        //for nse[]
+
+        st.push(n-1);
+        nse[n-1] = n;
+
+        for(int i=n-2; i>=0; i--){
+            while(st.size() > 0 && heights[st.peek()] >= heights[i]){
+                st.pop();
+            }
+
+            if(st.size() == 0) nse[i] = n;
+            else nse[i] = st.peek();
+
+            st.push(i);
+        }
+
+        while(st.size() > 0) st.pop();
+
+        //for pse
+
+        st.push(0);
+        pse[0] = -1;
+
+        for(int i=1; i<n; i++){
+            while(st.size() > 0 && heights[st.peek()] >= heights[i]) st.pop();
+
+            if(st.size() == 0) pse[i] = -1;
+            else pse[i] = st.peek();
+
+            st.push(i);
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++){
+            int area = heights[i]*(nse[i] - pse[i] - 1);
+
+            max = Math.max(max, area);
+        }
+
+        return max;
+    }
+}
