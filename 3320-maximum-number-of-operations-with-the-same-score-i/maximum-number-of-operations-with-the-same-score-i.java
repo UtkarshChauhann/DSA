4fa2e5fb51952cc96cd0104d37c2ead1e294dd53
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2025-11-22 14:20:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (19.46%) - LeetSync

---
+++ 3320-maximum-number-of-operations-with-the-same-score-i/maximum-number-of-operations-with-the-same-score-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxOperations(int[] nums) {
+        int cnt = 1;
+        int sum = nums[0]+nums[1];
+
+        for(int i=2; i<nums.length-1; i+=2){
+            if(nums[i] + nums[i+1] == sum) cnt++;
+            else break;
+        }
+        return cnt;
+    }
+}
