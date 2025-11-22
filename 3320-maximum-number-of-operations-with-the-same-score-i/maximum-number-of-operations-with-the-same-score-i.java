de30ca8831732e9e34b0042e335032fc82c1b619
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2025-11-22 14:20:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (14.05%) - LeetSync

--- 3320-maximum-number-of-operations-with-the-same-score-i/maximum-number-of-operations-with-the-same-score-i.java
+++ 3320-maximum-number-of-operations-with-the-same-score-i/maximum-number-of-operations-with-the-same-score-i.java
@@ -3,7 +3,7 @@ class Solution {
         int cnt = 1;
         int sum = nums[0]+nums[1];
 
-        for(int i=2; i<nums.length-1; i+=2){
+        for(int i=2; i<nums.length-1; i+=2 ){ //increment i by 2
             if(nums[i] + nums[i+1] == sum) cnt++;
             else break;
         }
