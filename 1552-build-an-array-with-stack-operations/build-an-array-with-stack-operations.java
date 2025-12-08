e8c0c4184dce0ed645c079aa3d8a77055478e3b8
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2025-12-08 22:21:57 +0530

    Time: 1 ms (17.68%) | Memory: 44.5 MB (6.92%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -1,19 +1,23 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        List<String> ans = new ArrayList();
-        int i = 1;
-        
-        for (int num : target) {
-            while (i < num ) {
-                ans.add("Push");
-                ans.add("Pop");
-                i++;
-            }
-            
+
+        Stack<Integer> st = new Stack<>();
+        List<String> ans = new ArrayList<>();
+
+        int i = 1, idx = 0;
+
+        while(idx < target.length){
+            st.push(i);
             ans.add("Push");
+
+            if(i != target[idx]){
+                st.pop();
+                ans.add("Pop");
+            }
+            else idx++;
             i++;
         }
-        
+
         return ans;
     }
 }
