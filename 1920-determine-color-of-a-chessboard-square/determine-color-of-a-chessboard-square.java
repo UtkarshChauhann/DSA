fcea0394b0368895e25fdd89be07c417ceb1a7c2
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2025-12-10 22:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (13.13%) - LeetSync

--- 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.java
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.java
@@ -1,12 +1,6 @@
 class Solution {
-    public boolean squareIsWhite(String coordinates) {
-        char[] ch = coordinates.toCharArray();
-
-        int n1 = (int) ch[0];
-        int n2 = (int) ch[1];
-
-        if(n1 % 2 != 0 && n2 % 2 != 0 || n1 % 2 == 0 && n2 % 2 == 0) return false;
-
-        return true;
+    public boolean squareIsWhite(String a) {
+        
+        return a.charAt(0) % 2 != a.charAt(1) % 2; 
     }
 }
