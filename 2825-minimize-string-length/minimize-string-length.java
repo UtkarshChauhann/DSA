296c64894dbddb98c18bc3ee50125fa3f372b79a
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2025-12-21 00:07:15 +0530

    Time: 3 ms (99.79%) | Memory: 46.4 MB (60.95%) - LeetSync

--- 2825-minimize-string-length/minimize-string-length.java
+++ 2825-minimize-string-length/minimize-string-length.java
@@ -1,11 +1,12 @@
 class Solution {
     public int minimizedStringLength(String s) {
-        Set<Character> set = new HashSet<>();
-
-        for(char ch: s.toCharArray()){
-            set.add(ch);
-        }
-
-        return set.size();
+        boolean[] friend = new boolean[26];
+        for (char c : s.toCharArray())
+            friend[c - 'a'] = true;
+        int cnt = 0;
+        for (int i = 0; i < 26; ++i)
+            if (friend[i])
+                cnt++;
+        return cnt;
     }
 }
