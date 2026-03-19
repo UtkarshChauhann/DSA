24dd1b573145150cb440732c54f20294bf167966
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2026-03-19 18:51:58 +0530

    Time: 7 ms (52.00%) | Memory: 161.3 MB (64.80%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -4,16 +4,14 @@ class Solution {
         int m = grid[0].length;
 
         int cnt = 0;
-
+        //prefix sum
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(i>0 && j>0) grid[i][j] += grid[i-1][j] + grid[i][j-1] - grid[i-1][j-1];
+                if(j > 0) grid[i][j] += grid[i][j-1];
 
-                else if(j > 0) grid[i][j] += grid[i][j-1];
+                if(i > 0) grid[i][j] += grid[i-1][j];
 
-                else if(i > 0) grid[i][j] += grid[i-1][j];
-
-               
+                if(i>0 && j>0) grid[i][j] -= grid[i-1][j-1]; //sub overlapping diagonal
 
                 if(grid[i][j] <= k) cnt++;
             }
