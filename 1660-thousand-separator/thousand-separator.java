c31e59c67ea47ab05067563c489d94a0ef1c1655
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2026-05-27 21:53:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (43.34%) - LeetSync

---
+++ 1660-thousand-separator/thousand-separator.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String thousandSeparator(int n) {
+        StringBuilder s = new StringBuilder(String.valueOf(n));
+
+        for(int i=s.length()-3; i>0; i=i-3){
+            s.insert(i, ".");
+        }
+
+        return s.toString();
+    }
+}
