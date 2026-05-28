2fcf49300003445050d666c9ddf91f321f135158
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2026-05-28 22:44:31 +0530

    Time: 29 ms (6.99%) | Memory: 47.6 MB (9.05%) - LeetSync

--- 4313-password-strength/password-strength.java
+++ 4313-password-strength/password-strength.java
@@ -1,29 +1,26 @@
 class Solution {
     public int passwordStrength(String password) {
 
-        Set<Character> lower = new HashSet<>();
-        Set<Character> upper = new HashSet<>();
-        Set<Character> digit = new HashSet<>();
-        Set<Character> special = new HashSet<>();
+        Set<Character> s = new HashSet<>();
 
         int ans = 0;
 
         for (char ch : password.toCharArray()) {
 
-            if (Character.isLowerCase(ch) && !lower.contains(ch)) {
-                lower.add(ch);
+            if (Character.isLowerCase(ch) && !s.contains(ch)) {
+                s.add(ch);
                 ans += 1;
             }
-            else if (Character.isUpperCase(ch) && !upper.contains(ch)) {
-                upper.add(ch);
+            else if (Character.isUpperCase(ch) && !s.contains(ch)) {
+                s.add(ch);
                 ans += 2;
             }
-            else if (Character.isDigit(ch) && !digit.contains(ch)) {
-                digit.add(ch);
+            else if (Character.isDigit(ch) && !s.contains(ch)) {
+                s.add(ch);
                 ans += 3;
             }
-            else if ((ch == '!' || ch == '@' || ch == '#' || ch == '$') && !special.contains(ch)) {
-                special.add(ch);
+            else if ((ch == '!' || ch == '@' || ch == '#' || ch == '$') && !s.contains(ch)) {
+                s.add(ch);
                 ans += 5;
             }
         }
