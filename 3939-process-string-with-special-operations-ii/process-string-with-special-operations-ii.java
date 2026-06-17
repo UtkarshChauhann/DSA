1b6ee456c87f2b89775dd70a14611c7b0e852b53
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2026-06-17 17:07:26 +0530

    Time: 28 ms (88.46%) | Memory: 47.5 MB (96.15%) - LeetSync

--- 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
@@ -1,8 +1,14 @@
 class Solution {
     public char processStr(String s, long k) {
+
+        // Pass 1:
+        // Compute only the final length of the string.
+        // We never build the actual string because its length
+        // can be as large as 1e15.
         long len = 0;
 
-        for(char ch: s.toCharArray()){
+        for (char ch : s.toCharArray()) {
+
             if(ch == '*'){ 
                 if(len > 0) len--;
             }
@@ -14,25 +20,49 @@ class Solution {
             else len++;
         }
 
-        if(k >= len) return '.';
+        // k is outside the final string
+        if (k >= len) return '.';
 
-        for(int i=s.length()-1; i>=0; i--){  
+        // Pass 2:
+        // Walk backwards and keep converting k
+        // to the index it came from before each operation.
+        for (int i = s.length() - 1; i >= 0; i--) {
+
             char ch = s.charAt(i);
 
-            if(ch == '#'){
+            if (ch == '#') {
+                // Forward: abc -> abcabc
+                // Backward: First restore old length.
                 len /= 2;
-                if(k >= len) k = k - len;
+
+                // If k lies in the second copy, map it to the corresponding index in the first copy.
+                if (k >= len) {
+                    k = k - len;
+                }
             }
 
-            else if(ch == '*') len++;
+            else if (ch == '*') {
+                // Forward: abc -> ab
+                // Backward: Restore length before deletion. No need to modify k because surviving characters keep same indices.
 
-            else if(ch == '%'){
-                k = len-k-1;
+                len++;
             }
 
-            else{
+            else if (ch == '%') {
+                // Forward: abc -> cba
+                // Backward: Reverse index mapping.
+                k = len - k - 1;
+            }
+
+            else {
+
+                // Forward: oldString + ch
+                // This character was appended at index = oldLength.
                 len--;
-                if(k == len) return ch;
+
+                if (k == len) {
+                    return ch;
+                }
             }
         }
 
