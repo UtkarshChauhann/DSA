bd788edd010cf3e951f348966d2cad41fe4ae978
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2026-07-12 19:22:33 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (92.36%) - LeetSync

---
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    private int toSeconds(String time) {
+        int hours = Integer.parseInt(time.substring(0, 2));
+        int minutes = Integer.parseInt(time.substring(3, 5));
+        int seconds = Integer.parseInt(time.substring(6, 8));
+
+        return hours * 3600 + minutes * 60 + seconds;
+    }
+
+    public int secondsBetweenTimes(String startTime, String endTime) {
+        return toSeconds(endTime) - toSeconds(startTime);
+    }
+}
