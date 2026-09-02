f8cb828c779f908511ad56e3b640f3386ca3f748
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2026-09-02 20:03:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (17.43%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,15 +1,17 @@
-class Solution {
-    public int memo(int i, int[] dp){
-        if(i<=2) return i;
-
-        if(dp[i] != -1) return dp[i];
-
-        return dp[i] = memo(i-1, dp) + memo(i-2, dp);
-    }
+class Solution { //tabu
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
+        if (n == 0 || n == 1) {
+            return 1;
+        }
 
-        return memo(n, dp);
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 }
