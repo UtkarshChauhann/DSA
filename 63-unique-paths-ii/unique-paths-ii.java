ec3e2147aad79a8c139ed264a21d09ae5664a55c
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2026-09-02 22:52:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (11.29%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,28 +1,28 @@
 class Solution {
-    public int memo(int i, int j, int[][] obs, int[][] dp){
-        if(i < 0 || j < 0) return 0;
-
-        if(obs[i][j] == 1) return 0;
-        if(i == 0 && j == 0) return 1; 
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int row = memo(i-1, j, obs, dp);
-        int col = memo(i, j-1, obs, dp);
-
-        return dp[i][j] = row + col;
-    }
     public int uniquePathsWithObstacles(int[][] obs) {
         int m = obs.length;
         int n = obs[0].length;
 
-        int[][] dp = new int[m+1][n+1];
+        int[][] dp = new int[m][n];
 
-        for(int[] row: dp){
-            Arrays.fill(row, -1);
+        dp[0][0] = 1;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                if(obs[i][j] == 1) { // Obstacle
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                if(i > 0)
+                    dp[i][j] += dp[i-1][j];
+
+                if(j > 0)
+                    dp[i][j] += dp[i][j-1];
+            }
         }
 
-        return memo(m-1, n-1, obs, dp);
+        return dp[m-1][n-1];
     }
-    
 }
