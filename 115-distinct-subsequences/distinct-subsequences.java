a6bf201f3bc7c71efd6951be33588d0849e029a9
Utkarsh Chauhan <137974898+UtkarshChauhann@users.noreply.github.com>
2026-09-06 22:05:05 +0530

    Time: 19 ms (69.08%) | Memory: 54.2 MB (62.97%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public int memo(int i, int j, String s, String t, int[][] dp) {
+
+        if(j < 0) return 1;
+
+        if(i < 0) return 0;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        if(s.charAt(i) == t.charAt(j)) {
+
+            int take = memo(i - 1, j - 1, s, t, dp);
+            int notTake = memo(i - 1, j, s, t, dp);
+
+            return dp[i][j] = take + notTake;
+        }
+
+        return dp[i][j] = memo(i - 1, j, s, t, dp);
+    }
+
+    public int numDistinct(String s, String t) {
+
+        int n = s.length();
+        int m = t.length();
+
+        int[][] dp = new int[n][m];
+
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return memo(n - 1, m - 1, s, t, dp);
+    }
+}
